dce2c79e06bb01dc3b3c63ea69892994cd92322b
Alex Motion <152202621+AlxMotion@users.noreply.github.com>
2023-11-30 11:19:12 -0600

    Update PlaylistNode.h

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,8 +1,8 @@
 #ifndef PLAYLISTNODE_H
 #define PLAYLISTNODE_H
-
 #include <string>
 using namespace std;
+
 class PlaylistNode {
 public:
     PlaylistNode();  
@@ -15,5 +15,17 @@ public:
     int GetSongLength() const;
     PlaylistNode* GetNext() const;
 
-    
+    void InsertAfter(PlaylistNode* nodePtr);
+    void SetNext(PlaylistNode* nodePtr);
+    void PrintPlaylistNode() const;
+
+private:
+    string uniqueID;
+    string songName;
+    string artistName;
+    int songLength;
+    PlaylistNode* nextNodePtr;
+};
+
+#endif
 
